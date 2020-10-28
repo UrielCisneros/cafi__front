cce8aa361f252103d6bc7c7391d665948f161324
poday98 <poday736.98@gmail.com>
2020-10-28 01:02:25 -0600

    ajusteResponsivo

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -20,8 +20,8 @@ import {
 export default function Footer() {
     return (
             <div className="container-fluid principal my-4">
-                <div className="row ">
-                    <div className="mx-auto col-lg-3 mt-3 ">
+                <div className="row text-center">
+                    <div className="d-none d-sm-none d-md-block col-lg-3 mt-3 mx-auto">
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="100" width="250" alt="" />
                     </div>
 
