f7235affb0903857dcb626b527919b05673c992f
poday98 <poday736.98@gmail.com>
2020-11-13 12:32:05 -0600

    vistas

--- src/pages/users/capacitaciones/capacitaciones.js
+++ src/pages/users/capacitaciones/capacitaciones.js
@@ -12,31 +12,149 @@ export default function Capacitaciones() {
 			<div className="container-fluid">
 				<div className="row">
 					<div className="col-lg-12">
-						<TweenOne className="cap-titulo mt-5" animation={{ y: 30, opacity: 0, type: 'from', delay: 500 }}>
-							Descubre todas nuestras capacitaciones que tenemos para ti!
+						<TweenOne  animation={{ y: 30, opacity: 0, type: 'from', delay: 500 }}>
+							<h1 className="cap-titulo mt-5">Descubre todas nuestras capacitaciones que tenemos para ti!</h1>
 						</TweenOne>
 					</div>
 
-					<div className="container-fluid">
-						<div className="row ">
+					<div className="container mt-5 ">
+						<div className="row">
 
-						<div className="card-video mx-auto mt-4">
-							<iframe  width="250px" height="180px"
-								className="front-video"
-								src="https://www.youtube.com/embed/P4Bda6_usuc" 
-								frameborder="0" 
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-								allowfullscreen>
-							</iframe>
-							<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
-								Titulo de video
-							</TweenOne>
-							
-							<p className="descripcion-video text-center">
-								Descripcion de video educativo 
-							</p>
-						</div>
+							<div className="card-video mx-auto mt-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
 
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-4 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
+
+							<div className="card-video mx-auto mt-5 mr-5">
+								<iframe  width="240px" height="170px"
+									className="front-video"
+									src="https://www.youtube.com/embed/P4Bda6_usuc" 
+									frameborder="0" 
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+									allowfullscreen>
+								</iframe>
+								<TweenOne className="titulo-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Titulo de video
+								</TweenOne>
+								
+								<TweenOne className="descripcion-video text-center" animation={{ y: 30, opacity: 0, type: 'from' , delay: 800}}>
+									Descripcion de video educativo 
+								</TweenOne>
+							</div>
 						
 						</div>
 					</div>
